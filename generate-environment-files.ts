402edb913f05936f12cd98132f22f7d174d3d5bf
Folusho Oladipo <fdoladipo@gmail.com>
2021-10-26 17:25:14 +0100

    Generate one env filesfor Angular, not multiple

--- generate-environment-files.ts
+++ generate-environment-files.ts
@@ -4,23 +4,21 @@ import {writeFileSync} from "fs"
 loadEnv()
 
 const {
-  API_DOMAIN,
-  API_DOMAIN_PROD,
-  NODE_ENV,
+  API_DOMAIN: apiDomain,
 } = process.env
-const isProd = NODE_ENV === "production"
-const apiDomain = isProd ? API_DOMAIN_PROD : API_DOMAIN
+
 if (!apiDomain) {
-  throw new Error(`Error. You forgot to specify ${isProd ? "API_DOMAIN_PROD" : "API_DOMAIN"}.`)
+  throw new Error("Error. You forgot to specify API_DOMAIN.")
 }
 
-const targetPath = isProd ? "./src/environments/environment.prod.ts" : "./src/environments/environment.ts"
-
+const targetPath = "./src/environments/environment.ts"
 const envContent = `export const environment = {
   production: false,
   appName: "Metagram",
   apiHost: "${apiDomain}/api/v0"
-};
+}
 `
 
 writeFileSync(targetPath, envContent)
+
+console.log(`Finished generating ${targetPath}\n\n`)
